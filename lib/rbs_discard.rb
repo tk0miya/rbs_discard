5f91522f843b377eb18b3593e24ca1fe866ac34a
Takeshi KOMIYA <i.tkomiya@gmail.com>
2023-08-27 14:36:30 +0900

    Initial commit (Just after "bundle gem rbs_discard")

---
+++ lib/rbs_discard.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rbs_discard/version"
+
+module RbsDiscard
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rbs_discard/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RbsDiscard
+  VERSION = "0.1.0"
+end
