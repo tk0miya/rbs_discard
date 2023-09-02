f2a3fd85a81ee324a20eaef7f77006be3de42ecf
Takeshi KOMIYA <i.tkomiya@gmail.com>
2023-09-03 01:15:22 +0900

    refactor: Clean up generated rake task

--- lib/generators/rbs_discard/install_generator.rb
+++ lib/generators/rbs_discard/install_generator.rb
@@ -6,8 +6,11 @@ module RbsDiscard
   class InstallGenerator < ::Rails::Generators::Base
     def create_raketask
       create_file "lib/tasks/rbs_discard.rake", <<~RUBY
+        # frozen_string_literal: true
+
         begin
           require "rbs_discard/rake_task"
+
           RbsDiscard::RakeTask.new
         rescue LoadError
           # failed to load rbs_discard. Skip to load rbs_discard tasks.
